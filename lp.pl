0bf866888ec46999030ae75f715cf54c4afda410
Sudheesh Bhattarai <sudheeshbhattarai@Sudheeshs-MacBook-Pro.local>
2017-02-06 22:53:11 -0500

    Test cases works and added comment.

--- lp.pl
+++ lp.pl
@@ -2,35 +2,25 @@
 
 Question 1
 
-Write a predicate sum-up-numbers-simple(L, N). L is a list, which may
-contain as elements numbers and non-numbers. The predicate is true if N is the sum of the
-numbers not in nested lists in L. If there are no such numbers, the result is zero.
-
-[] 0 True
-[100,200] 300 True
-[a,b,c] 0 True
-[100,a] 100 True
-[a,100] 100 True
-[a,100,b,200,c,300,d] 600 True
-[[]] 0 True
-[[100]] 0 True
-[100,[200]] 100 True
-[a,100,b,[200],c,300,d] 400 True
+There are 3 cases. 
+1) If the list is empty, return 0.
+2) If the first number is not number, we recursively call the function by adding 0.
+3) Else if number, we add the first element and the rest of the list.
 */
 
 
 
-sum-up-numbers-simple([],0).
+sum-up-numbers-simple([],0). //if the list is empty
 
-sum-up-numbers-simple([X|Y], N):-
-	not(number(X)),
+sum-up-numbers-simple([X|Y], N):- 
+	not(number(X)), //if the first element is not a number
 	sum-up-numbers-simple(Y, Sum),
-	N is 0 + Sum.
+	N is 0 + Sum. //we add zero
 
-sum-up-numbers-simple([X|Y], N):-
-	number(X),
+sum-up-numbers-simple([X|Y], N):- 
+	number(X),   //if the first element is a number
 	sum-up-numbers-simple(Y, Sum),
-	N is X + Sum.
+	N is X + Sum.   //we add the first element
 
 
 
