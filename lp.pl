f99cba5e4795d4846cc314dffb1bb25a5436682e
Sudheesh Bhattarai <sudheeshbhattarai@Sudheeshs-MacBook-Pro.local>
2017-02-06 23:04:34 -0500

    Question 2. Adding the same code as question 1.

--- lp.pl
+++ lp.pl
@@ -10,17 +10,53 @@ There are 3 cases.
 
 
 
-sum-up-numbers-simple([],0). //if the list is empty
+sum-up-numbers-simple([],0). %if the list is empty
 
 sum-up-numbers-simple([X|Y], N):- 
-	not(number(X)), //if the first element is not a number
+	not(number(X)), %if the first element is not a number
 	sum-up-numbers-simple(Y, Sum),
-	N is 0 + Sum. //we add zero
+	N is 0 + Sum. %we add zero
 
 sum-up-numbers-simple([X|Y], N):- 
-	number(X),   //if the first element is a number
+	number(X),   %if the first element is a number
 	sum-up-numbers-simple(Y, Sum),
-	N is X + Sum.   //we add the first element
+	N is X + Sum.   %we add the first element
+
+
+
+/*
+Question 2
+
+Write a predicate sum-up-numbers-general(L, N). L is a list, which may
+contain as elements numbers and non-numbers. The predicate is true if N is the sum of all
+the numbers (including those in nested lists) in L. If there are no such numbers, the result is
+zero.
+
+L N Result
+[] 0 True
+[100] 100 True
+[100,200] 300 True
+[a] 0 True
+[a,100,b,200,c,300,d] 600 True
+[[]] 0 True
+[[100]] 100 True
+[100,[200]] 300 True
+[a,100,b,[200],c,300,d] 600 True
+[a,100,[[b,[[200],c]],300,d]] 600 True
+
+*/
+
+sum-up-numbers-general([],0). 
+
+sum-up-numbers-general([X|Y], N):- 
+	not(number(X)), 
+	sum-up-numbers-general(Y, Sum),
+	N is 0 + Sum. 
+
+sum-up-numbers-general([X|Y], N):- 
+	number(X),  
+	sum-up-numbers-general(Y, Sum),
+	N is X + Sum.  
 
 
 
